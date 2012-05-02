26e9030ad2b784da227dd3a06e372fc9ea868b6b
KM Tu <tutu522@gmail.com>
2012-05-03 00:23:55 +0900

    Use stringstream to read in data.
    I am not satisfied with this. Too many new and delete operations. I am thinking to go by the old way.

--- src/voronoi.cpp
+++ src/voronoi.cpp
@@ -1,11 +1,15 @@
 #include <argp.h> /* argument parse */
-#include <iostream>
+#include <iostream> /* cin cout cerr */
+#include <fstream>
 #include <vector>
 #include <string>
+#include <cstdlib> /* exit() */
+#include <sstream> /* stringstream */
 //#include "voro++.hh"
 
 //using namespace voro;
 using namespace std;
+
 
 const char* argp_program_version = "Voronoi 0.1";
 const char* argp_program_bug_address = "kmtu@nmr.kuicr.kyoto-u.ac.jp";
@@ -14,21 +18,19 @@ const char* argp_program_bug_address = "kmtu@nmr.kuicr.kyoto-u.ac.jp";
 static char doc[] = "Voronoi anaylsis";
 
 /* A description of the arguments we accept. */
-static char args_doc[] = "FILE...";
+static char args_doc[] = "dataFile(s)...";
 
 /* The options we understand. */
 static struct argp_option options[] = {
-    {"output",   'o', "FILE", 0,  "Output to FILE instead of standard output" },
-
+    {"output",   'o', "outFile", 0,  "Output file." },
     { 0 }
 };
 
 /* Used by main to communicate with parse_opt. */
 struct arguments
 {
-//    char** strings;               /* [string...] */
-    vector<string> strings;
-    string output_file;            /* file arg to ‘--output’ */
+    vector<string> dataFilenames;
+    string outFilename;            /* file arg to ‘--output’ */
 };
 
 /* Parse a single option. */
@@ -37,37 +39,18 @@ static error_t parse_opt(int key, char* arg, struct argp_state* state)
     /* Get the input argument from argp_parse, which we
       know is a pointer to our arguments structure. */
     struct arguments* arguments = static_cast<struct arguments*> (state->input);
-//    struct arguments* arguments = (struct arguments*) (state->input);
 
     switch(key)
     {
     case 'o':
-       arguments->output_file = arg;
+       arguments->outFilename = arg;
        break;
 
-//    case ARGP_KEY_NO_ARGS:
-//       argp_usage (state);
+    case ARGP_KEY_NO_ARGS:
+       argp_usage (state);
 
     case ARGP_KEY_ARG:
-       /* Here we know that state->arg_num == 0, since we
-          force argument parsing to end before any more arguments can
-          get here. */
-//       arguments->arg1 = arg;
-
-       /* Now we consume all the rest of the arguments.
-          state->next is the index in state->argv of the
-          next argument to be parsed, which is the first string
-          we're interested in, so we can just use
-          &state->argv[state->next] as the value for
-          arguments->strings.
-
-          In addition, by setting state->next to the end
-          of the arguments, we can force argp to stop parsing here and
-          return. */
-//       arguments->strings = &(state->argv[state->next-1]);
-       arguments->strings.push_back(state->argv[state->next - 1]);
- //      state->next = state->argc;
-
+       arguments->dataFilenames.push_back(arg);
        break;
 
     default:
@@ -82,31 +65,74 @@ static struct argp argp = { options, parse_opt, args_doc, doc };
 
 int main(int argc, char** argv) {
     struct arguments arguments;
+    string line;
 
-    /* Default values. */
-//    arguments.strings = NULL;
-///    arguments.output_file = "-";
+    /* Default values for arguments*/
+    arguments.outFilename = "voronoi.out";
 
     /* Parse our arguments; every option seen by parse_opt will be
       reflected in arguments. */
     argp_parse (&argp, argc, argv, 0, 0, &arguments);
 
-//     error (10, 0, "ABORTED");
+    /* Show the parsed arguments on the screen */
+    cout << "outFile name = " << arguments.outFilename << endl;
 
-    if (arguments.strings.size() > 0)
+    if (arguments.dataFilenames.size() > 0)
     {
-        cout << "STRINGS = ";
-        for (vector<string>::iterator it = arguments.strings.begin();
-             it != arguments.strings.end(); it++)
-            cout << *it << ' ';
+        cout << "dataFile name(s) = ";
+        for (vector<string>::iterator it = arguments.dataFilenames.begin();
+             it != arguments.dataFilenames.end(); it++)
+        {
+            if (it == arguments.dataFilenames.begin())
+                cout << *it;
+            else
+                cout << ", " << *it;
+        }
+        cout << endl;
     }
     else
     {
-        cout << "STRINGS = NULL";
+        cerr << " Error: dataFile(s) must be given ";
+        exit(1);
+    }
+    
+    /* Check outFile's availabilty */
+    ofstream outFile(arguments.outFilename.c_str());
+    if (!outFile.is_open())
+    {
+        cerr << "Error: unable to open outFile, " << arguments.outFilename << endl;
+        exit(1);
+    }
+    
+    /* Iterate and open each dataFile */
+    stringstream* pssline;
+    for (vector<string>::iterator it = arguments.dataFilenames.begin();
+         it != arguments.dataFilenames.end(); it++)
+    {
+        ifstream dataFile(it->c_str());
+        if (dataFile.is_open())
+        {
+            cout << "Reading " << *it << " ..." << endl;
+            while (dataFile.good())
+            {
+                /* Read and process dataFile */
+                getline(dataFile, line);
+                pssline = new stringstream(line);
+                // *pssline >> var1 >> var2
+                cout << pssline->str() << endl;
+                delete pssline;
+            }
+            dataFile.close();
+        }
+        else
+        {
+            cerr << "Error: unable to open file: " << *it << endl;
+            exit(1);
+        }
     }
 
-    cout << endl << "OUTPUT_FILE = " << arguments.output_file << endl;
 
+    outFile.close();
     return 0;
 }
 
