8dd67a2e20e4a9a3e44d67e9f071cb0db1f3b474
KM Tu <tutu522@gmail.com>
2012-05-07 03:42:20 +0900

    Complete version 1.
    Now it can output all the voronoi cell volumes to a file. The stringstream issue is also fixed. I learned that one should call clean() first before assigning a new string to the stringstream object.

--- src/voronoi.cpp
+++ src/voronoi.cpp
@@ -5,12 +5,13 @@
 #include <string>
 #include <cstdlib> /* exit() */
 #include <sstream> /* stringstream */
-//#include "voro++.hh"
+#include <cmath> /* pow() */
+#include "voro++.hh"
 
-//using namespace voro;
+using namespace voro;
 using namespace std;
 
-
+/********** argp start **********/
 const char* argp_program_version = "Voronoi 0.1";
 const char* argp_program_bug_address = "kmtu@nmr.kuicr.kyoto-u.ac.jp";
 
@@ -59,9 +60,9 @@ static error_t parse_opt(int key, char* arg, struct argp_state* state)
     return 0;
 }
 
-
 /* Our argp parser. */
 static struct argp argp = { options, parse_opt, args_doc, doc };
+/********** argp end **********/
 
 int main(int argc, char** argv) {
     struct arguments arguments;
@@ -104,11 +105,19 @@ int main(int argc, char** argv) {
         exit(1);
     }
     
-    /* Iterate and open each dataFile */
-    stringstream* pssline;
+    /* Go through each dataFile */
+    stringstream ssline;
     for (vector<string>::iterator it = arguments.dataFilenames.begin();
          it != arguments.dataFilenames.end(); it++)
     {
+        int numFrames = 0;
+        int numAtoms = 0;
+        int dummy = 0;
+        int lineIndex = 0;
+        int numUnitCellPoints = 0;
+        int numAtomsPerMolecule = 0;
+
+        /* Create dataFile object for input*/
         ifstream dataFile(it->c_str());
         if (dataFile.is_open())
         {
@@ -116,11 +125,106 @@ int main(int argc, char** argv) {
             while (dataFile.good())
             {
                 /* Read and process dataFile */
+                lineIndex++;
                 getline(dataFile, line);
-                pssline = new stringstream(line);
-                // *pssline >> var1 >> var2
-                cout << pssline->str() << endl;
-                delete pssline;
+                ssline.clear();
+                ssline.str(line);
+
+                if (lineIndex == 1)
+                {
+                    ssline >> numFrames >> dummy >> dummy >> numAtoms;
+                    cout << "numFrames = " << numFrames << endl;
+                    cout << "numAtoms = " << numAtoms << endl;
+                }
+                else if (lineIndex == 9)
+                {
+                    /* Only the first type of molecules are analyzed */
+                    ssline >> numUnitCellPoints;
+                    cout << "numUnitCellPoints = " << numUnitCellPoints << endl;
+                }
+                else if (lineIndex == 10)
+                {
+                    ssline >> numAtomsPerMolecule;
+                    cout << "numAtomsPerMolecule = " << numAtomsPerMolecule << endl;
+    
+                    /* Read data frame by frame */
+                    for (int iFrame = 0; iFrame < 10; iFrame++)
+                    {
+                        double boxDimX = .0; 
+                        double boxDimY = .0;
+                        double boxDimZ = .0;
+                        const int NUM_GRID_X = 6;
+                        const int NUM_GRID_Y = 6;
+                        const int NUM_GRID_Z = 6;
+
+                        /* Read box dimension */
+                        getline(dataFile, line);
+                        ssline.clear();
+                        ssline.str(line);
+                        ssline >> boxDimX;
+
+                        getline(dataFile, line);
+                        ssline.clear();
+                        ssline.str(line);
+                        ssline >> boxDimY >> boxDimY;
+
+                        getline(dataFile, line);
+                        ssline.clear();
+                        ssline.str(line);
+                        ssline >> boxDimZ >> boxDimZ >> boxDimZ;
+
+                        // Create a container with the geometry given above, and make it
+                        // periodic in x and y coordinates. Allocate space for
+                        // eight particles within each computational block
+                        container con(-boxDimX/2., boxDimX/2., -boxDimY/2., boxDimY/2., -boxDimZ/2., boxDimZ/2., 
+                                      NUM_GRID_X, NUM_GRID_Y, NUM_GRID_Z,
+                                      true, true, false, 8);
+
+                        for (int iMolecule = 0; iMolecule < numUnitCellPoints; iMolecule++)
+                        {
+                            getline(dataFile, line); // Skip 1st C atom
+
+                            int ix = 0, iy = 0, iz = 0;
+                            double rx = 0., ry = 0., rz = 0.;
+                            getline(dataFile, line);
+                            ssline.clear();
+                            ssline.str(line);
+                            ssline >> ix >> iy >> iz;
+
+                            rx = static_cast<double>(ix) * 500. / pow(2., 30);
+                            ry = static_cast<double>(iy) * 500. / pow(2., 30);
+                            rz = static_cast<double>(iz) * 500. / pow(2., 30);
+                            con.put(iMolecule, rx, ry, rz);
+
+                            /* Skip the rest atoms in the molecule */
+                            for (int i = 0; i < numAtomsPerMolecule - 2; i++)
+                            {
+                                getline(dataFile, line);
+                            }
+                        }
+
+                        /* Output volume info for each cell */
+                        c_loop_all loop(con);
+                        voronoicell vcell;
+                        if (!loop.start())
+                        {
+                            cerr << "Error occurred while starting the voro cell loop.";
+                            exit(1);
+                        }
+
+                        do
+                        {
+                            con.compute_cell(vcell, loop);
+                            outFile << vcell.volume() << endl;
+                        }while(loop.inc());
+
+                        /* Skip other non Voronoi atoms */
+                        for (int i = 0; i < numAtoms - numAtomsPerMolecule * numUnitCellPoints; i++)
+                        {
+                            getline(dataFile, line);
+                        }
+                    }
+                }
             }
             dataFile.close();
         }
@@ -130,7 +234,6 @@ int main(int argc, char** argv) {
             exit(1);
         }
     }
-
 
     outFile.close();
     return 0;
