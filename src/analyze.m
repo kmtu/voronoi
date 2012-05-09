f385e273e7e74aa4b6c18cef957da048facd2b06
KM Tu <tutu522@gmail.com>
2012-05-10 00:02:47 +0900

    Support manual selection of errorbars (S.D.) positions

--- src/analyze.m
+++ src/analyze.m
@@ -3,25 +3,32 @@
 clear all
 format long
 
+
+if (nargin() < 3)
+    error("Usage: $analyze.m baseFileName imin imax [errbar pos...]")
+endif
+
 baseFileName = argv(){1}
 imin = str2num(argv(){2})
 imax = str2num(argv(){3})
 indexes = [imin:imax]
 
 if (nargin() > 3)
-    opt = argv(){4} #even or odd
+    errbarVolumes_cell = argv()(4:end)
+    for i = [1:length(errbarVolumes_cell)]
+        errbarVolumes(i) = str2num(errbarVolumes_cell{i})
+    endfor
 endif
 
 for i = indexes
     fileName = strcat(baseFileName, num2str(i, "%02d"))
     data = load(fileName)
-    if (i == 1)
+    if (!exist("volume"))
         volume = data(:,1)
         distribution = data(:,2)
     else
         if (volume != data(:,1))
             error("The volume indexes of file %s is different", fileName)
-            exit(1)
         endif
         distribution(:, end+1) = data(:,2)
     endif
@@ -31,15 +38,17 @@ distribution_mean = mean(distribution, 2);
 distribution_std = std(distribution, 0, 2);
 
 if (nargin() > 3)
-    #even
-    if (strcmp(opt, "even"))
-        distribution_std(1:2:end) = 0
-    endif
+    for i = [1:length(errbarVolumes)]
+        #find the closest volume, both its value and index
+        [m, mi] = min(abs(errbarVolumes(i) - volume))
+        errbarIndexes(i) = mi
+    endfor
 
-    #odd
-    if (strcmp(opt, "odd"))
-        distribution_std(2:2:end) = 0
-    endif
+    for i = [1:length(volume)]
+        if (all(errbarIndexes != i))
+            distribution_std(i) = 0
+        endif
+    endfor
 endif
 
 out = [volume, distribution_mean, distribution_std]
--- src/analyze.sh
+++ src/analyze.sh
@@ -1,5 +1,6 @@
 volumeDir="volume-data"
-binvolumeDir="binvolume-data"
+numBins=500
+binvolumeDir="binvolume-data/bin${numBins}"
 
 function getbin()
 {
@@ -7,9 +8,8 @@ function getbin()
     pressure=$1
     begin=$2
     end=$3
-    numBins=1000
     min=0
-    max=52000
+    max=50000
     for ((i=$begin; i<=$end; i=i+1))
     do 
         ii=$(printf "%02d" $i)
@@ -25,32 +25,35 @@ function getstd()
     begin=$2
     end=$3
     outFilename=$4
-    ./analyze.m ${binvolumeDir}/binvolume-${pressure}_1ns- $begin $end
+    errbars="2000 5000 10000 15000 20000 25000 30000 35000 40000 45000"
+    ./analyze.m ${binvolumeDir}/binvolume-${pressure}_1ns- $begin $end $errbars
     mv ${stdBaseFilename}$begin-$end $outFilename
 }
 
+runGetbin="false"
+
 pressure="001atm"
-#getbin $pressure 1 9
-b=2; e=9;
-getstd $pressure $b $e "binvolume_std-${pressure}_1ns-${b}_${e}"
+[ "$runGetbin" == "true" ] && getbin $pressure 1 10
+b=1; e=10;
+getstd $pressure $b $e "binvolume_std-${pressure}_1ns_bin${numBins}-${b}_${e}"
 
 pressure="200atm"
-##getbin $pressure 1 25
+[ "$runGetbin" == "true" ] && getbin $pressure 1 25
 b=16; e=25;
-getstd $pressure $b $e "binvolume_std-${pressure}_1ns-${b}_${e}"
+getstd $pressure $b $e "binvolume_std-${pressure}_1ns_bin${numBins}-${b}_${e}"
 
 pressure="400atm"
-#getbin $pressure 1 25
+[ "$runGetbin" == "true" ] && getbin $pressure 1 25
 b=16; e=25;
-getstd $pressure $b $e "binvolume_std-${pressure}_1ns-${b}_${e}"
+getstd $pressure $b $e "binvolume_std-${pressure}_1ns_bin${numBins}-${b}_${e}"
 
 pressure="001a200"
-#getbin $pressure 1 16
+[ "$runGetbin" == "true" ] && getbin $pressure 1 16
 b=7; e=16;
-getstd $pressure $b $e "binvolume_std-${pressure}_1ns-${b}_${e}"
+getstd $pressure $b $e "binvolume_std-${pressure}_1ns_bin${numBins}-${b}_${e}"
 
 pressure="001a400"
-#getbin $pressure 1 20
+[ "$runGetbin" == "true" ] && getbin $pressure 1 20
 b=11; e=20;
-getstd $pressure $b $e "binvolume_std-${pressure}_1ns-${b}_${e}"
+getstd $pressure $b $e "binvolume_std-${pressure}_1ns_bin${numBins}-${b}_${e}"
 
